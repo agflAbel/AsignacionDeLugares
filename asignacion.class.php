b44cd88a7c0f76d1c0118e24327fa8f89e05dd71
agflAbel <agflabel@gmail.com>
2025-03-16 03:40:06 -0600

    Primera version

--- asignacion.class.php
+++ asignacion.class.php
@@ -8,8 +8,14 @@ class Asigna {
     function asignar($lugares,$grupos){
         $nL = count($lugares);
         $nG = count($grupos);
+        $i=0;
+        foreach($grupos as $grupo):
+            $final[$i][0]=$grupo;
+            $final[$i][1]=$lugares[random_int(0,$nL-1)];
+            $i++;
+        endforeach;
         
-        return $nL;
+        return $final;
     }
 }
 ?>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if($action):
     // print_r($_POST);
     // print("-----------");
     $final=$a->asignar($lugares,$grupos);
-    print_r($final);
+    
 endif;
 ?>
 <!DOCTYPE HTML>
@@ -19,13 +19,17 @@ endif;
 		<title>Asignacion de lugares</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<link rel="stylesheet" href="assets/css/miestilo.css" />
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jacquard+24&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/main.css" />
 	</head>
     <body>
         <header>
-
+        <img src="img/image.png" class="img-fluid enc" alt="...">
+        <h1 class="titulo">Asignacion de Lugares</h1>
+        <!--pre><?php print_r($final);?></pre>-->
         </header>
         <div class="container">
             <form id="f" class="align-items-center" method="post" enctype="multipart/form-data" action="index.php?action=asignar">
@@ -40,10 +44,10 @@ endif;
                     </div>
                     <div class="row">
                         <div class="col">
-                            <textarea class="form-control" name="lugares" id="lugares" rows="4"></textarea>
+                            <textarea class="form-control" name="lugares" id="lugares" rows="4"><?php echo(isset($final))?$_POST['lugares']:'' ?></textarea>
                         </div>
                         <div class="col">
-                            <textarea class="form-control" name="grupos" id="lugares" rows="4"></textarea>
+                            <textarea class="form-control" name="grupos" id="lugares" rows="4"><?php echo(isset($final))?$_POST['grupos']:'' ?></textarea>
                         </div>
                     </div>
                     <div class="row">
@@ -63,16 +67,23 @@ endif;
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                    $i=1;
+                    foreach($final as $asig):
+                    ?>
                     <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
+                    <th scope="row"><?php print $i?></th>
+                    <td><?php print $asig[0] ?></td>
+                    <td><?php print $asig[1] ?></td>
                     </tr>
+                    <?php $i++; endforeach;?>
                 </tbody>
             </table>
         </div>
     </body>
     <footer>
-
+        <p>
+            Desarrollado por: Aguilar Flores Abel
+        </p>
     </footer>
 </html>
